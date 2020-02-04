1c03814ae2060a2e3a25468a6345b3be54909f6c
mikahilantoshkin <mike.antoshkin@gmail.com>
2020-02-04 14:20:40 +0500

    Some cleanup and addition of check for supported FS

--- FSReader/FSReaderClient.cpp
+++ FSReader/FSReaderClient.cpp
@@ -8,29 +8,62 @@
 #include <algorithm>
 #include "FSReader.h"
 
+
 using namespace std;
 
 
 
 int main()
 {
-
+    
     BYTE bBootSector[512];
     memset(bBootSector, 0, 512);
     DWORD dwBytesRead(0);
     BPB _bpb;
-    LPWSTR lpDrive;
-    // {0} ~ ZeroMemory()
-    PNTFS_VOLUME_DATA_BUFFER ntfsVolData = { 0 };
-    // NTFS_EXTENDED_VOLUME_DATA versionMajMin = {0};
-    BOOL bDioControl = FALSE;
-    DWORD dwWritten = 0;
-    HANDLE hDisk = CreateFileA("\\\\.\\C:",
+    string diskName;
+       
+    char NameBuffer[MAX_PATH];
+    char SysNameBuffer[MAX_PATH];
+    DWORD VSNumber;
+    DWORD MCLength;
+    DWORD FileSF;
+    
+    // TODO: validation
+    while (1){
+        cout << "Please specify volume name. Should be like \"C\", \"D\", and so on" << endl;
+        diskName = cin.get();
+        if (diskName.length() == 1 ) {
+            break;
+        }
+    }
+
+    
+    string diskNameFormated = "\\\\.\\" + diskName + ":";
+    string forVolumeInf = diskName + ":\\";
+    
+
+    if (GetVolumeInformationA(forVolumeInf.c_str(), NameBuffer, sizeof(NameBuffer),
+        &VSNumber, &MCLength, &FileSF, SysNameBuffer, sizeof(SysNameBuffer)))
+    {
+        cout << "Detected file system is " << SysNameBuffer << endl;
+    }
+    string SysName = SysNameBuffer;
+
+    if (SysName.find("NTFS") == string::npos) {
+        cout << "Sorry, but "<< SysName << " is not supported yet!" << endl;
+        cout << "Press any key to exit" << endl;
+        //needs to be called twice for some reason
+        cin.get();
+        cin.get();
+        exit(0);
+    }
+
+    HANDLE hDisk = CreateFileA(diskNameFormated.c_str(),
         GENERIC_READ, FILE_SHARE_READ | FILE_SHARE_WRITE | FILE_SHARE_DELETE,
         NULL, OPEN_EXISTING, 0, NULL);
     if (hDisk == INVALID_HANDLE_VALUE)
     {
-        wprintf(L"CreateFile() failed! Proboly run as administrator\n");
+        wprintf(L"CreateFile() failed! You'll rrobably need to run the app as administrator\n");
         wprintf(L" %u \n", GetLastError());
         if (CloseHandle(hDisk) != 0)
             wprintf(L"hVolume handle was closed successfully!\n");
@@ -40,38 +73,17 @@ int main()
         }
         exit(1);
     }
-    else
-        wprintf(L"CreateFile() is pretty fine!\n");
+     
     if (!ReadFile(hDisk, bBootSector, 512, &dwBytesRead, NULL))
     {
-        printf("Error in reading floppy disk\n");
+        printf("Error in reading the disk\n");
     }
     else
     {
-        // Print floppy information on Console.
         CloseHandle(hDisk);
-     /*   BYTE temp;
-        for (unsigned int a = 1; a < sizeof(Offsets) / sizeof(Offsets[0]); a = a + 1) {
-            int revIndx  = Offsets[a - 1];
-            int arrIndx = Offsets[a]-1;
-            int section_len = arrIndx - arrIndx;
-            while (revIndx < arrIndx) {
-                temp = bBootSector[revIndx];
-                bBootSector[revIndx] = bBootSector[arrIndx];
-                bBootSector[arrIndx] = temp;
-
-                revIndx++;
-                arrIndx--;
-                }
-
-            BYTE* start = &bBootSector[Offsets[a - 1]];
-            BYTE* end = &bBootSector[Offsets[a]-1];
-            
-        }*/
         memcpy(&_bpb, bBootSector, 512);
-        
         PrintBootSectInfo(_bpb);
-        cout << "Press any key to exit";
+        cout << "Press Enter to exit";
         getchar();
     }
 }
